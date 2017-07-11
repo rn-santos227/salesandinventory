ce4a96f99cf7f1437766048b184221eae7b4b2c7
zsanagustin <zsanagustin4@gmail.com>
2017-07-11 13:45:30 +0800

    updated sidenav

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('layouts.sidenav')
     <h1>Hello Wurld</h1>
 @endsection
--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -3,64 +3,78 @@
 <div class="nav-side-menu">
     <div class="brand">Brand Logo</div>
     <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
-  
+    
         <div class="menu-list">
-  
-            <ul id="menu-content" class="menu-content collapse out">
-                <li>
-                  <a href="#">
-                  <i class="fa fa-dashboard fa-lg"></i> Dashboard
-                  </a>
-                </li>
+  <ul id="menu-content" class="menu-content collapse out">
+    <li data-toggle="collapse" data-target="#home" class="collapsed active">
+      <a href="#">
+        <i class="fa fa-home fa-lg"></i>  Home <span class="arrow"></span>
+      </a>
+    </li>
+    <ul class="sub-menu collapse" id="home">
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Messages</a></li>
+      <li><a href="#">Task</a></li>
+      <li><a href="#">Notes</a></li>
+      <li><a href="#">Account Settings</a></li>
+      <li><a href="#">Logout</a></li>
+    </ul>
+    <li data-toggle="collapse" data-target="#overview" class="collapsed active">
+      <a href="#">
+        <i class="fa fa-briefcase fa-lg"></i>  Overview <span class="arrow"></span>
+      </a>
+    </li>
+    <ul class="sub-menu collapse" id="overview">
+      <li><a href="#">Company</a></li>
+      <li><a href="#">Policies</a></li>
+      <li><a href="#">Accounts</a></li>
+      <li><a href="#">Audit Trail</a></li>
+      <li><a href="#">System Setting</a></li>
+    </ul>
+    <li data-toggle="collapse" data-target="#sales" class="collapsed active">
+      <a href="#">
+        <i class="fa fa-shopping-cart fa-lg"></i>  Sales <span class="arrow"></span>
+      </a>
+    </li>
+    <ul class="sub-menu collapse" id="sales">
+      <li><a href="#">Restaurant Mode</a></li>
+      <li><a href="#">Retailer Mode</a></li>
+      <li><a href="#">Delivery Mode</a></li>
+    </ul>
 
-                <li  data-toggle="collapse" data-target="#products" class="collapsed active">
-                  <a href="#"><i class="fa fa-gift fa-lg"></i> UI Elements <span class="arrow"></span></a>
-                </li>
-                <ul class="sub-menu collapse" id="products">
-                    <li class="active"><a href="#">CSS3 Animation</a></li>
-                    <li><a href="#">General</a></li>
-                    <li><a href="#">Buttons</a></li>
-                    <li><a href="#">Tabs & Accordions</a></li>
-                    <li><a href="#">Typography</a></li>
-                    <li><a href="#">FontAwesome</a></li>
-                    <li><a href="#">Slider</a></li>
-                    <li><a href="#">Panels</a></li>
-                    <li><a href="#">Widgets</a></li>
-                    <li><a href="#">Bootstrap Model</a></li>
-                </ul>
-
-
-                <li data-toggle="collapse" data-target="#service" class="collapsed">
-                  <a href="#"><i class="fa fa-globe fa-lg"></i> Services <span class="arrow"></span></a>
-                </li>  
-                <ul class="sub-menu collapse" id="service">
-                  <li>New Service 1</li>
-                  <li>New Service 2</li>
-                  <li>New Service 3</li>
-                </ul>
-
-
-                <li data-toggle="collapse" data-target="#new" class="collapsed">
-                  <a href="#"><i class="fa fa-car fa-lg"></i> New <span class="arrow"></span></a>
-                </li>
-                <ul class="sub-menu collapse" id="new">
-                  <li>New New 1</li>
-                  <li>New New 2</li>
-                  <li>New New 3</li>
-                </ul>
-
-
-                 <li>
-                  <a href="#">
-                  <i class="fa fa-user fa-lg"></i> Profile
-                  </a>
-                  </li>
-
-                 <li>
-                  <a href="#">
-                  <i class="fa fa-users fa-lg"></i> Users
-                  </a>
-                </li>
-            </ul>
-     </div>
+    <li data-toggle="collapse" data-target="#products" class="collapsed active">
+      <a href="#">
+        <i class="fa fa-list fa-lg"></i>  Inventory <span class="arrow"></span>
+      </a>
+    </li>
+    <ul class="sub-menu collapse" id="products">
+      <li><a href="items">Products</a></li>
+      <li><a href="#">Customers</a></li>
+      <li><a href="#">Suppliers</a></li>
+      <li><a href="#">Categories</a></li>
+      <li><a href="#">Archives</a></li>
+    </ul>
+    <li data-toggle="collapse" data-target="#reports" class="collapsed active">
+      <a href="#">
+        <i class="fa fa-clipboard fa-lg"></i>  Reports <span class="arrow"></span>
+      </a>
+    </li>
+    <ul class="sub-menu collapse" id="reports">
+      <li><a href="#">Documents</a></li>
+      <li><a href="#">Expenditures</a></li>
+      <li><a href="#">Invoices</a></li>
+    </ul>
+    <li data-toggle="collapse" data-target="#analytics" class="collapsed active">
+      <a href="#">
+        <i class="fa fa-bar-chart fa-lg"></i>  Analytics <span class="arrow"></span>
+      </a>
+    </li>
+    <ul class="sub-menu collapse" id="analytics">
+      <li><a href="#">Supply Purchase Request</a></li>
+      <li><a href="#">Supply Acquisition</a></li>
+      <li><a href="#">Supply Status</a></li>
+      <li><a href="#">Depreciation / Expiration</a></li>
+    </ul>
+  </ul>
+</div>
 </div>
