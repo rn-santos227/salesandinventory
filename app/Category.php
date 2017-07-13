bcc5ce09c4382dc0165e365c9de05ac9b32e4611
rn-santos227 <rn_santos227@yahoo.com>
2017-07-13 14:08:55 +0800

    Remove Conflict

--- app/Category.php
+++ app/Category.php
@@ -9,8 +9,6 @@ class Category extends Model
     protected $fillable = [
         'ref_code', 'name', 'description',
     ];
-<<<<<<< HEAD
-=======
 
     public function setActiveAttribute($value) {
         $this->attributes['active'] = $value == 'Active' ? 1 : 0;
@@ -19,5 +17,4 @@ class Category extends Model
     public function getActiveAttribute() {
         return $this->attributes['active'] ? 'Active' : 'Inactive';
     }
->>>>>>> refs/remotes/origin/master
 }
