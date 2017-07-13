6f9ed866d5b4b0945509887144d3ebd80d6fa5ec
zsanagustin <zsanagustin4@gmail.com>
2017-07-13 16:13:13 +0800

    e

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.app')
-
 @section('content')
+    @include('layouts.menu')
     
-<div class="row">
-    <div class="col-md-3 content-fluid">
-        @include('admin.sidenav')
-    </div>
-    <div class="col-md-9 content-fluid">
-        admin
-    </div>
-</div>
 @endsection
