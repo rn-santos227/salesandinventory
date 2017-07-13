b749ce1cec7a2d018e07df2aeea9cdad6827ca77
janarielsanjose <janarielsanjose@gmail.com>
2017-07-13 16:23:49 +0800

    Modified Supplier Blades (Added dismiss)

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -6,7 +6,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Suppliers</div>
                 <div class="panel-body">
-                    <button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target="#create">
+                    <button type="button" class="btn btn-success pull-right" data-toggle="modal" data-target="#create">
                     <span class="hidden-xs hidden-sm">New Item</span>
                     </button><br><br>
                     <table class="table table-bordered table-responsive">
@@ -25,7 +25,7 @@
                                 <td>{{$supplier->name}}</td>
                                 <td>{{$supplier->active}}</td>
                                 <td style="width: 225px;">
-                                    <button class="btn btn-info" style="width: 100px;" data-toggle="modal" data-target="#view{{$supplier->id}}">View</button>
+                                    <button class="btn btn-primary" style="width: 100px;" data-toggle="modal" data-target="#view{{$supplier->id}}">View</button>
                                     <button class="btn btn-warning" style="width: 100px;" data-toggle="modal" data-target="#update{{$supplier->id}}">Edit</button>
 <!--                                    <button class="btn btn-danger" style="width: 100px;">Remove</button>-->
                                 </td>
