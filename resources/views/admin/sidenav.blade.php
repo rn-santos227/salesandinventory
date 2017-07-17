f75b4e985886892314c1a44e9be220fc203d45f3
zsanagustin <zsanagustin4@gmail.com>
2017-07-17 09:42:57 +0800

    s

--- resources/views/admin/sidenav.blade.php
+++ resources/views/admin/sidenav.blade.php
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <div class="nav-side-menu">
     <div class="brand">Brand Logo</div>
     <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
     
         <div class="menu-list">
-=======
-<div class="menu-list">
->>>>>>> refs/remotes/origin/master
+
   <ul id="menu-content" class="menu-content collapse out">
     <li data-toggle="collapse" data-target="#home" class="collapsed active">
       <a href="#">
